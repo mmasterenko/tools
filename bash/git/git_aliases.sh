c0b9cfc3985b4a9285af52235976b73959926366
Maksim Masterenko <mmasterenko@gmail.com>
2022-11-28 10:55:31 +0300

    :rocket: bash tools

---
+++ bash/git/git_aliases.sh
@@ -0,0 +1,7 @@
+alias gst='git status'
+alias gi='git commit'
+alias gl='git log --oneline'
+alias ga='git add .'
+alias gh='git log --graph --pretty=format:"%Cred %h %Cblue [%aD] %Cgreen %s %Cblue [%an] %Cred %d"'
+alias gb='git branch'
+
